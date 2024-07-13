9afc16b509d82e086827764d6b49b24931e13148
Kristanto Sean <kristantosean@gmail.com>
2024-07-13 12:47:45 +0700

    Initial Commit

---
+++ BodyPoseWithMVVM/BodyPoseWithMVVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  BodyPoseWithMVVMApp.swift
+//  BodyPoseWithMVVM
+//
+//  Created by Kristanto Sean on 13/07/24.
+//
+
+import SwiftUI
+
+@main
+struct BodyPoseWithMVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
